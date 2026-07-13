27095d7832f6486c7799658143bc2a0cd5272448
jianghaojia <2908487619@qq.com>
2026-07-13 16:46:16 +0800

    Initial commit

---
+++ _git_push.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "c:\Users\Administrator\Desktop\node练习"
+git add .
+git commit -m "Initial commit"
+git push -u origin main
---
+++ _git_push.ps1
@@ -0,0 +1,4 @@
+Set-Location "c:\Users\Administrator\Desktop\node练习"
+git add .
+git commit -m "Initial commit"
+git push -u origin main
